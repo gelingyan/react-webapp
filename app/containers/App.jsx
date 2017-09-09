2ba1f83e7af86b182142661bfa955d72b77f75f8
gelingyan <gely@jikon.cn>
2017-09-10 00:48:25 +0800

    性能优化插件react-addons-pure-render-mixin

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 class App extends React.Component {
     constructor(props, context) {
         super(props, context);
-    //    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate()
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
+
         this.state = {
             initDone: false
         }
